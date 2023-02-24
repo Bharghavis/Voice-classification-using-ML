279cc3023926a314a1bb1778de6615f48cd8d119
Bharghavi.S <91484473+Bharghavis@users.noreply.github.com>
2023-02-24 16:46:25 +0530

    app1.y

--- app1.py
+++ app1.py
@@ -27,7 +27,7 @@ def get_file_content_as_string(path):
     
 @st.cache(show_spinner=False)
 def load_model():
-    model=tf.keras.models.load_model('cnnmodel.h5')
+    model=tf.keras.models.load_model('autoencoder.h5')
     
     return model
 def application():
